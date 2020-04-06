bcc81cfb7897048871b7d0ed4bc554fecf2abaf1
Shuang Chen <gechenshuang88@gmail.com>
2020-04-06 16:29:46 +0200

    adjust test parameters

--- base_module/bcs_tespy.py
+++ base_module/bcs_tespy.py
@@ -53,8 +53,8 @@ def consumer_demand(t):  # dynamic thermal demand from consumer
     # thermal demand in each month (assumed specific heat extraction rate*
     # length of BHE* number of BHE)
     month_demand = [
-        -25 * 50 * 3, -25 * 50 * 3, -25 * 50 * 3, -25 * 50 * 3, -25 * 50 * 3,
-        -25 * 50 * 3, -25 * 50 * 3, -25 * 50 * 3, -25 * 50 * 3, -25 * 50 * 3,
+        -25 * 50 * 25, -25 * 50 * 25, -25 * 50 * 25, -25 * 50 * 25, -25 * 50 * 25,
+        -25 * 50 * 25, -25 * 50 * 25, -25 * 50 * 25, -25 * 50 * 25, -25 * 50 * 25,
     ]
     return month_demand[t_trans - 1]
 
